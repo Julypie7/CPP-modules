3176ccc7850cb7b234b0493300df9f4a47a380cd
Iuliia Neimatulina <ineimatu@cbr3s1.42barcelona.com>
2025-05-14 22:13:43 +0200

    CPP02/ex00 main

---
+++ CPP2/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ineimatu <ineimatu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 22:12:57 by ineimatu          #+#    #+#             */
+/*   Updated: 2025/05/14 22:13:25 by ineimatu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
