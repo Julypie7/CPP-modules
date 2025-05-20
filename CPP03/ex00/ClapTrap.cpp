e37ef83999f162b588f195b72cf8518bfb2e8832
ineimatu <ineimatu@student.42.fr>
2025-05-20 08:23:45 +0200

    ex00

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -3,11 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ineimatu <ineimatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:23:19 by ineimatu          #+#    #+#             */
-/*   Updated: 2025/05/06 20:23:21 by ineimatu         ###   ########.fr       */
+/*   Updated: 2025/05/20 08:17:07 by ineimatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(){
+    std::cout << "Default constructor is called!" << std::endl;
+    _hitPoints = 10;
+    _energyPoints = 10;
+    _attackDamage = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy){
+    std::cout << "Copy costructor is called!" << std::endl;
+    *this=copy;
+}
+
+ClapTrap::ClapTrap(std::string name){
+    std::cout << "String constructor is called!" << std::endl;
+    _name = name;
+    _hitPoints = 10;
+    _energyPoints = 10;
+    _attackDamage = 0;
+}
+
+ClapTrap::~ClapTrap(){
+    std::cout << "Destructor is called!" << std::endl;
+}
+
+void    ClapTrap::setName(std::string name){
+    _name = name;
+}
+
+void   ClapTrap::attack(const std::string &target){
+    if (_hitPoints != 0 && _energyPoints != 0){
+        std::cout << "ClapTrap " << _name << " attacks" << target << ", causing" << _attackDamage << " points of damage!" << std::endl;
+        _energyPoints--;
+    }
+    else if (_hitPoints <= 0)
+        std::cout << "ClapTrap " << _name << " cannot attacks, it doesnt have enough hit points!" << std::endl;
+    else
+        std::cout << "ClapTrap " << _name << " cannot attacks, it doesnt have enough energy points!" << std::endl;
+    
+}
+
+void ClapTrap::takeDamage(unsigned int ammount){
+    if (_hitPoints > 0 && _energyPoints != 0){
+        std::cout << "ClapTrap " << _name << " was attacked, loosing" << ammount << " hit points!" << std::endl;
+        _hitPoints -= ammount;
+        if (_hitPoints < 0)
+            _hitPoints = 0;
+    }
+    else
+        std::cout << "ClapTrap " << _name << " was attacked, but cannot take any more damage!" << std::endl;
+}
 
--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ineimatu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ineimatu <ineimatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:23:28 by ineimatu          #+#    #+#             */
-/*   Updated: 2025/05/06 20:28:20 by ineimatu         ###   ########.fr       */
+/*   Updated: 2025/05/20 08:01:56 by ineimatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,25 @@
 #include <iostream>
 #include <string>
 
-Class ClapTrap {
+class ClapTrap {
 	private:
 		std::string _name;
-		int hitPoints = 10;
-		int energyPoints = 10;
-		int attackDamage = 10;
+		int _hitPoints;
+		int _energyPoints;
+		int _attackDamage;
 
 	public:
+		ClapTrap();
+		ClapTrap(const std::string name);
+		ClapTrap(const ClapTrap &copy);
+		ClapTrap &operator=(const ClapTrap &copy);
+		~ClapTrap();
+		
+		void setName(std::string name);
+		
 		void attack(const std::string& target);
 		void takeDamage(unsigned int ammount);
 		void beRepaired(unsigned int ammount);
-}
+};
+
+#endif
